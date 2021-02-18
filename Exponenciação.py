9cb63ba8422d5128ea7d3fbbbbaeea88f82bf9de
LuanRamalho <32373552+LuanRamalho@users.noreply.github.com>
2021-02-18 14:31:49 -0300

    Add files via upload

---
+++ Exponenciação.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+print("Digite um número inteiro: ")
+num = int(input())
+
+print("Exponenciação de 1: ",num**1)
+print("Exponenciação de 2: ",num**2)
+print("Exponenciação de 3: ",num**3)
+print("Exponenciação de 4: ",num**4)
+print("Exponenciação de 5: ",num**5)
+print("Exponenciação de 6: ",num**6)
+print("Exponenciação de 7: ",num**7)
+print("Exponenciação de 8: ",num**8)
+print("Exponenciação de 9: ",num**9)
+print("Exponenciação de 10: ",num**10)
+
+    
